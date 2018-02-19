ae70ddd8c5c202b270eacd7cccec7061cfea7e70
AmyangXYZ <amyang.xyz@gmail.com>
2018-02-19 14:41:09 +0800

    add config

--- config/config.go
+++ config/config.go
@@ -3,5 +3,5 @@ package config
 const (
 	RootDir   = "/go/src/github.com/AmyangXYZ/SG_Amyang"
 	SecretKey = "biubiubiu"
-	DB        = "root:0311@tcp(172.17.0.1:3306)/sg_amyang?charset=utf8"
+	DB        = "root:0311@tcp(localhost:3306)/sg_amyang?charset=utf8"
 )
